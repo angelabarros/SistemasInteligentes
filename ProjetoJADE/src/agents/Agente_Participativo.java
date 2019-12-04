84835dc156c2b1d99b8ddd5577168d8220d28232
Angela Barros <angelabarros29@hotmail.com>
2019-12-04 22:23:41 +0000

    Revert "passar info para a interface"
    This reverts commit 6a173c590ac658b35f3a635fb37d430e3f3cb243.

--- ProjetoJADE/src/agents/Agente_Participativo.java
+++ ProjetoJADE/src/agents/Agente_Participativo.java
@@ -251,7 +251,6 @@ public class Agente_Participativo extends Agent {
 				ex.printStackTrace();
 			}
 			msg.addReceiver(receiver);
-			msg.addReceiver(receiver_interface);
 			myAgent.send(msg);
 
 		}
